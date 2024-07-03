5f50c4e4b1c8f70a66aaa6973100692f0f960449
Le Dang Huy <huyg.a22052003@gmail.com>
2024-07-03 18:32:48 +0700

    add skill fire punch

--- Demo/Assets/Scripts/PlayerController.cs
+++ Demo/Assets/Scripts/PlayerController.cs
@@ -207,6 +207,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    public void OnCastFire(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            animator.SetTrigger(AnimationStrings.castFireTrigger);
+        }
+    }
+
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
